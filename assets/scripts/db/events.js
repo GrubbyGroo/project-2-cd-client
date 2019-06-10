f0c520c52a9d43bc91caa6d6437e79893801c227
DiGregorioC <digregorioc@outlook.com>
2019-06-10 09:20:15 -0400

    Add delete function to games
    Add delete method to games, owned by signed in user

--- assets/scripts/db/events.js
+++ assets/scripts/db/events.js
@@ -16,15 +16,23 @@ const onCreateGame = event => {
   event.preventDefault()
   const form = (event.target)
   const formData = getFormFields(form)
-
   api.createGame({game_list: formData})
     .then(ui.onCreateSuccess)
+    .catch(ui.onCreateFailure)
+}
+
+const onDeleteGame = (event) => {
+  event.preventDefault()
+  const gameId = $(event.target).closest('section').data('id')
+  api.deleteGame(gameId)
+    .then(() => onGetGames(event))
     .catch(ui.onCreateFailure)
 }
 
 const dbHandlers = () => {
   $('#getGames').on('click', onGetGames)
   $('#createGame').on('submit', onCreateGame)
+  $('.content').on('click', '.delete-game', onDeleteGame)
 }
 
 module.exports = {
